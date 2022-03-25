19d5ff4c7a01be01c1c703d4f9842029fd793483
VSProgramme <38361226+VincentSinel@users.noreply.github.com>
2022-03-25 17:15:11 +0100

    Ajout Collision Jeu Moto

--- Programmes/JeuMoto/Moto.js
+++ Programmes/JeuMoto/Moto.js
@@ -13,12 +13,19 @@ class Moto extends Lutin
         this.OldY = 0;
 
         this.CentreRotation = new Vecteur2(24/155, 67.5/93)
+        this.Immortel = 60;
     }
 
 
     Calcul(Delta)
     {
         super.Calcul(Delta)
+
+        if (this.Mort)
+        {
+            this.AnimationMort(Delta);
+            return;
+        }
 
         this.X = this.OldX;
         this.Y = this.OldY;
@@ -53,7 +60,7 @@ class Moto extends Lutin
         this.Direction = coef;
 
         // Déplacement de la camera
-        Camera.X = Math.max(this.X, Ecran_Largeur / 2);
+        Camera.X = Math.max(this.X + 300, Ecran_Largeur / 2)
         
         this.OldX = this.X;
         this.OldY = this.Y;
@@ -62,8 +69,8 @@ class Moto extends Lutin
         let H = (this.Zoom / 100) * this.Image.height;
 
         // Assignation de la coordonnée Z
-        let decalageX = (24 / 155) * H;
-        let decalageY = (25.5 / 93) * L;
+        let decalageX = this.CentreRotation.X * L;
+        let decalageY = (1 - this.CentreRotation.Y) * H;
         this.Z = - this.Y + decalageY; 
 
 
@@ -71,46 +78,66 @@ class Moto extends Lutin
 
         Debug.AjoutVecteur(
             new Vecteur2(this.X - decalageX, this.Y - decalageY + H/3), 
-            new Vecteur2(0,0)
+            new Vecteur2(L,0)
             )
 
         Debug.AjoutVecteur(
             new Vecteur2(this.X - decalageX + L, this.Y - decalageY + H/3), 
-            new Vecteur2(0,0)
+            new Vecteur2(0,H/3)
             )
 
         Debug.AjoutVecteur(
             new Vecteur2(this.X - decalageX, this.Y - decalageY), 
-            new Vecteur2(0,0)
+            new Vecteur2(0,-H/3)
             )
 
         Debug.AjoutVecteur(
             new Vecteur2(this.X - decalageX + L, this.Y - decalageY), 
-            new Vecteur2(0,0)
+            new Vecteur2(-L,0)
             )
 
 
 
         //////////
         //let decalageX = (53.5 /155) * (this.Zoom / 100) * this.Image.width;
-        let IW =  this.Image.width * this.Zoom / 100;
-        let IH =  this.Image.height * this.Zoom / 100;
-        Debug.AjoutRectangle([this.X + decalageX - IW / 2, this.Y - decalageY + IH / 3, IW, IH / 3])
+        //let IW =  this.Image.width * this.Zoom / 100;
+        //let IH =  this.Image.height * this.Zoom / 100;
+        //Debug.AjoutRectangle([this.X - decalageX, this.Y - decalageY + H / 3, IW, H / 3])
 
-        this.TestCollision()
+        if (this.TestCollision() && this.Immortel <= 0)
+        {
+            this.Mort = true;
+        }
+        this.Dire(this.TestCollision());
 
+        this.Immortel -= 1
     }
     
-    
+    AnimationMort(Delta)
+    {
+        if (this.Direction + 2 < 180)
+        {
+            this.Direction += 10 * Delta;
+            this.TempsStop = 60;
+        }
+        else if (this.TempsStop > 0)
+        {
+            this.TempsStop -= Delta;
+        }
+        else{
+            this.Mort = false;
+            this.Immortel = 120;
+        }
+    }
 
 
     Collision()
     {
-        let decalageX = (53.5 /155) * (this.Zoom / 100) * this.Image.width;
-        let decalageY = (25.5 / 93) * (this.Zoom / 100) * this.Image.height;
-        let IW =  this.Image.width * this.Zoom / 100;
-        let IH =  this.Image.height * this.Zoom / 100;
-        return [this.X + decalageX - IW / 2, this.Y - decalageY + IH / 3, IW, IH / 3]
+        let L =  this.Image.width * this.Zoom / 100;
+        let H =  this.Image.height * this.Zoom / 100;
+        let decalageX = this.CentreRotation.X * L;
+        let decalageY = (1 - this.CentreRotation.Y) * H;
+        return [this.X - decalageX, this.Y - decalageY + H/3, L, H/3]
     }
 
     TestCollision()
--- Programmes/JeuMoto/Voiture.js
+++ Programmes/JeuMoto/Voiture.js
@@ -5,12 +5,12 @@ class Voiture extends Lutin
     {
         //let X = Voiture.Test * 50;
         //let Y = 100 - Voiture.Test;
-        let X = Math.random() * 5000
+        let X = 1000 + Math.random() * 50000
         let Y = Math.random() * 200 - 100
 
         super(X,Y,["Images/Moto/Voiture.png"])
 
-        this.Vitesse = 0;//1 + Math.random() * 5;
+        this.Vitesse = 1 + Math.random() * 5;
         this.Zoom = 50;
 
 
@@ -25,15 +25,15 @@ class Voiture extends Lutin
 
         this.Z = - this.Y 
         
-        let IW =  this.Image.width * this.Zoom / 100;
-        let IH =  this.Image.height * this.Zoom / 100;
-        Debug.AjoutRectangle([Camera.AdapteX(this.X - IW / 2), Camera.AdapteY(this.Y + IH / 2), IW, IH / 2])
+        let L =  this.Image.width * this.Zoom / 100;
+        let H =  this.Image.height * this.Zoom / 100;
+        Debug.AjoutRectangle([this.X - L / 2, this.Y + H / 2, L, H / 2])
     }
 
     Collision()
     {
-        let IW =  this.Image.width * this.Zoom / 100;
-        let IH =  this.Image.height * this.Zoom / 100;
-        return [this.X - IW / 2, this.Y + IH / 2, IW, IH / 2]
+        let L =  this.Image.width * this.Zoom / 100;
+        let H =  this.Image.height * this.Zoom / 100;
+        return [this.X - L / 2, this.Y + H / 2, L, H / 2]
     }
 }
